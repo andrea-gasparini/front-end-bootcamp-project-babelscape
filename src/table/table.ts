f80655c4e679fcb6260d0607e6fbb7ce30fcfbd7
Andrea Gasparini <gasptube@gmail.com>
2020-02-17 17:10:52 +0100

    Terminata configurazione di base componente Table

--- src/table/table.ts
+++ src/table/table.ts
@@ -17,16 +17,25 @@ export default class Table
     private render() : void
     {
         let table = $('<table />')
-            .addClass('table');
+            .addClass('table');   
+            
+        if (this._configuration.width !== undefined)
+            $(table).css('width', this._configuration.width + 'px');
 
-        
+        if (this._configuration.height !== undefined)
+            $(table).css('height', this._configuration.height + 'px');
 
         for ( let _row = 0; _row < this._configuration.data.length; _row++ )
         {
+            let tableRow = $('<tr />').appendTo(table);
+
             for ( let _col = 0; _col < this._configuration.data[0].length; _col++ )
-            {
-                console.log(this._configuration.data[_row][_col])
-            }
+                if ( _row === 0 && this._configuration.firstRowHeader )
+                    tableRow.append('<th>' + this._configuration.data[_row][_col] + '</th>');
+                else
+                    tableRow.append('<td>' + this._configuration.data[_row][_col] + '</td>');
         }
+
+        $(this._element).append(table)
     }
 }
