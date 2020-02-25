a8766a2429594dca07f80ab35ee06bba971f6d7e
Andrea Gasparini <gasptube@gmail.com>
2020-02-25 20:23:32 +0100

    Close responsive menu by clicking on #context

--- documentation/js/responsive-menu.js
+++ documentation/js/responsive-menu.js
@@ -9,5 +9,15 @@ $('document').ready(function()
             // .hide() aggiungerebbe display: none, così invece si cancella la proprietà
         else
             $('#menu').show();
-    })
+    });
+
+    $(document).click(function(event)
+    {
+        if ( $('#context').get(0).contains(event.target) && $('#menu-button').hasClass('change') )
+        {
+            $('#menu').hide();
+            $('#menu-button').toggleClass('change');
+
+        }
+    });
 })
