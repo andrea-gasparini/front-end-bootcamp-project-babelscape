a5c1e1b042e449150561b752fbb6e10062a372f0
Andrea Gasparini <gasptube@gmail.com>
2020-02-26 10:23:45 +0100

    Modularizzazione completa componente Dropdown

--- src/dropdown/dropdown-body.ts
+++ src/dropdown/dropdown-body.ts
@@ -1,10 +1,38 @@
-export default class DropdownBody
+import DropdownLabel from "./dropdown-label";
+import { DropdownType } from "../dropdown-type";
+import KeyValue, { keyValueToLowerCase } from "../key-value";
+
+export default class DropdownBody<T>
 {
     private _element : JQuery<HTMLUListElement> = $('<ul />');
+    private _values : Array<KeyValue> = new Array();
+    private _selectedValues : Array<KeyValue> = new Array();
+    private _dropdownType : DropdownType;
     private _bodyFilterElement : JQuery<HTMLInputElement> = $('<input />');
     private _liElements : Array<HTMLLIElement> = new Array();
+    private _dataMapper : (elem: T) => KeyValue;
+    private _onChange : (selectedList: Array<KeyValue>) => void;
+    private readonly _dropdownLabel : DropdownLabel<T>;
 
-    constructor()
+    constructor(dropdownLabel : DropdownLabel<T>, dropdownType : DropdownType, inputData : Array<T>, inputSelected : Array<string>, dataMapper : (elem: T) => KeyValue, onChange : (selectedList: Array<KeyValue>) => void)
+    {
+        for (let val of inputData)
+            this._values.push(dataMapper(val));
+
+        for (let selectedValue of inputSelected)
+            this._selectedValues.push(this._values.find((el) => el.key == selectedValue));
+
+        this._dropdownType = dropdownType;
+
+        this._dataMapper = dataMapper;
+        this._onChange = onChange;
+        this._dropdownLabel = dropdownLabel;
+        dropdownLabel.setDropdownBody(this);
+
+        this.render();
+    }
+
+    private render() : void
     {
         this._bodyFilterElement
             .prop('type', 'text')
@@ -14,15 +42,40 @@ export default class DropdownBody
         this._element
             .addClass('dropdown-body')
             .append(this._bodyFilterElement);
+
+        for (let val of this._values)
+            this.addLiElement(this.createliElement(val));
     }
 
-    addLiElement(liElement : HTMLLIElement) : void
+    private createliElement(val : KeyValue) : HTMLLIElement
+    {
+        let liElement = $('<li />')
+            .data('KeyValue', keyValueToLowerCase(val))
+            .text(val.value)
+            .on('click', (event : JQuery.ClickEvent) => this.updateSelection(liElement, event));
+
+        let isSelected = (this._dropdownType == DropdownType.MULTI) ?
+            this._selectedValues.find(el => el.key.toLowerCase() == val.key.toLowerCase()) :
+            this.selectedValues[0].key.toLowerCase() == val.key.toLowerCase();
+
+        if (isSelected) liElement.addClass('selected');
+
+        if (this._dropdownType == DropdownType.MULTI)
+            liElement
+                .prepend($('<input />')
+                    .prop('type', 'checkbox')
+                    .prop('checked', isSelected));
+
+        return liElement.get(0) as HTMLLIElement;
+    }
+
+    private addLiElement(liElement : HTMLLIElement) : void
     {
         this._liElements.push(liElement);
         this._element.append(liElement);
     }
 
-    filterSearch() : void
+    private filterSearch() : void
     {
         let input : string = this._bodyFilterElement.val() as string;
 
@@ -33,24 +86,97 @@ export default class DropdownBody
                 $(this._liElements[i]).hide();
     }
 
+    private updateSelection(liElement : JQuery<HTMLLIElement>, event : JQuery.ClickEvent) : void
+    {  
+        if (this._dropdownType == DropdownType.MULTI)
+        {
+            let checkBox = liElement.find('input');
+
+            if (checkBox.prop('checked'))
+            {
+                this._selectedValues = this._selectedValues.filter(el => el.value.toLowerCase() != liElement.data('KeyValue').value);
+                this._dropdownLabel.labelText = this._selectedValues.length == 0 ? this._dropdownLabel.placeholder : this._dropdownLabel.labelMapper(this._selectedValues);
+            }
+            else
+            {
+                this._selectedValues.push(this._values.find(el => el.value.toLowerCase() == liElement.data('KeyValue').value));
+                this._dropdownLabel.labelText = this._dropdownLabel.labelMapper(this._selectedValues);
+            }
+
+            if (event.target.tagName != 'INPUT')
+                checkBox.prop('checked', ! checkBox.prop('checked'));
+        }
+        else
+        {
+            $(this._liElements).removeClass('selected');
+            this._selectedValues = new Array(this._values.find(el => el.value.toLowerCase() == liElement.data('KeyValue').value));
+            this._dropdownLabel.labelText = this._dropdownLabel.labelMapper(this._selectedValues);
+            this.hide();
+        }
+
+        liElement.toggleClass('selected');
+
+        this._onChange(this._selectedValues);
+    }
+
+    setValues(values : Array<string>) : void
+    {
+        $(this._liElements).removeClass('selected');
+
+        if (this._dropdownType == DropdownType.MULTI)
+        {
+            $(this._liElements).find('input').prop('checked', false);
+
+            this._selectedValues = new Array();
+            let liElementsToSelect : HTMLLIElement[] = new Array();
+
+            for (let newValue of values)
+            {
+                this._selectedValues.push(this._values.find(el => el.key.toLowerCase() == newValue.toLowerCase()));
+                liElementsToSelect.push(this._liElements.filter(liElement => $(liElement).data('KeyValue').key == newValue.toLowerCase())[0]);
+            }
+
+            for (let liElement of liElementsToSelect)
+            {
+                let checkBox = $(liElement).find('input');
+                checkBox.prop('checked', true);
+                $(liElement).addClass('selected');
+            }
+
+            this._dropdownLabel.labelText = this._selectedValues.length == 0 ? this._dropdownLabel.placeholder : this._dropdownLabel.labelMapper(this._selectedValues);
+        }
+        else
+        {
+            this._selectedValues = new Array(this._values.find(el => el.key.toLowerCase() == values[0].toLowerCase()));
+            let liElementToSelect = this._liElements.filter(liElement => $(liElement).data('KeyValue').key == values[0].toLowerCase());;
+            $(liElementToSelect).addClass('selected');
+            this._dropdownLabel.labelText = this._dropdownLabel.labelMapper(this._selectedValues);
+        }        
+
+        this._onChange(this._selectedValues);
+    }
+
+    isVisible() : boolean { return this._element.is(':visible'); }
+
     show() : void { this._element.show(); }
 
     hide() : void { this._element.hide(); }
 
     toggle() : void { this._element.toggle(); }
 
-    get liElements() : Array<HTMLLIElement>
-    {
-        return this._liElements;
-    }
+    get values() : Array<KeyValue> { return this._values; }
 
-    get filterElement() : JQuery<HTMLInputElement>
-    {
-        return this._bodyFilterElement;
-    }
+    get selectedValues() : Array<KeyValue> { return this._selectedValues; }
 
-    get element() : JQuery<HTMLUListElement>
-    {
-        return this._element;
-    }
+    get type() : DropdownType { return this._dropdownType; }
+
+    get dataMapper() : (elem: T) => KeyValue { return this._dataMapper; }
+
+    get onChange() : (selectedList: Array<KeyValue>) => void { return this._onChange; }
+
+    get liElements() : Array<HTMLLIElement> { return this._liElements; }
+
+    get filterElement() : JQuery<HTMLInputElement> { return this._bodyFilterElement; }
+
+    get element() : JQuery<HTMLUListElement> { return this._element; }
 }
--- src/dropdown/dropdown-label.ts
+++ src/dropdown/dropdown-label.ts
@@ -1,24 +1,47 @@
-export default class DropdownLabel
+import { timingSafeEqual } from "crypto";
+import DropdownBody from "./dropdown-body";
+import KeyValue from "../key-value";
+import { DropdownType } from "../dropdown-type";
+
+export default class DropdownLabel<T>
 {
     private _element : JQuery<HTMLButtonElement> = $('<button />');
     private _placeholder : string;
     private _labelText : string;
     private _labelTextElement : JQuery<HTMLSpanElement> = $('<span />');
+    private _labelMapper : (selectedList: Array<KeyValue>) => string;
     private readonly _dropdownIcon : JQuery<HTMLElement> = $('<i class="fas fa-sort-down"></i>');
 
-    constructor(labelText : string, dropdownBodyElement : JQuery<HTMLElement>) 
+    constructor(placeholder : string, labelMapper : (selectedList: Array<KeyValue>) => string) 
     {
-        this.labelText = labelText;
+        this._placeholder = placeholder;
+        this._labelMapper = labelMapper;
+
+        this.render();
+    }
+
+    private render() : void
+    {
         this._element
-            .addClass('dropdown-label')
-            .append(this._dropdownIcon)
-            .append(this._labelTextElement)
-            .click(() =>
-                {
-                    dropdownBodyElement.toggle();
-                    if (dropdownBodyElement.is(':visible')) 
-                        dropdownBodyElement.find('input[type=text]').focus();
-                });
+        .addClass('dropdown-label')
+        .append(this._dropdownIcon)
+        .append(this._labelTextElement);
+    }
+
+    setDropdownBody(dropdownBody : DropdownBody<T>)
+    {
+        this.labelText = dropdownBody.selectedValues.length == 0 ? 
+            this._placeholder :
+            dropdownBody.type == DropdownType.MULTI ?
+                this._labelMapper(dropdownBody.selectedValues) :
+                this._labelMapper(new Array(dropdownBody.selectedValues[0]));
+
+        this._element.click(() =>
+            {
+                dropdownBody.toggle();
+                if (dropdownBody.isVisible())
+                    dropdownBody.filterElement.focus();
+            });
     }
 
     set labelText(labelText : string)
@@ -27,8 +50,13 @@ export default class DropdownLabel
         this._labelTextElement.text(labelText);
     }
 
-    get element() : JQuery<HTMLButtonElement>
-    {
-        return this._element;
-    }
+    get placeholder() : string { return this._placeholder; }
+
+    get labelText() : string { return this._labelText; }
+
+    get labelTextElement() : JQuery<HTMLSpanElement> { return this._labelTextElement; }
+
+    get labelMapper() : (selectedList: Array<KeyValue>) => string { return this._labelMapper; }
+
+    get element() : JQuery<HTMLButtonElement> { return this._element; }
 }
