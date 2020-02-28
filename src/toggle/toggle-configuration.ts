3b6b76520d4741d46326e524462a622b7740027a
Andrea Gasparini <gasptube@gmail.com>
2020-02-28 17:26:30 +0100

    Componente Toggle completato

--- src/toggle/toggle-configuration.ts
+++ src/toggle/toggle-configuration.ts
@@ -1,25 +1,33 @@
 import KeyValue from "../key-value";
-import Dimension from "../dimension";
 
 export default class ToggleConfiguration<T>
 {
     private _value1 : T;
     private _value2 : T;
-    private _dimensions : Dimension = new Dimension();
+    private _height : number = 25;
     private _dataMapper : (value : T) => KeyValue;
     private _onChange : (keyValue : KeyValue) => void;
 
-    constructor(config: {value1 : T, value2 : T, width? : number, height? : number, dataMapper : (value : T) => KeyValue, onChange})
+    constructor(config: {value1 : T, value2 : T, height? : number, dataMapper : (value : T) => KeyValue, onChange})
     {
         if (config.value1 !== undefined) this._value1 = config.value1;
         if (config.value2 !== undefined) this._value2 = config.value2;
 
-        if (config.width !== undefined) this._dimensions.width = config.width;
-        if (config.height !== undefined) this._dimensions.height = config.height;
+        if (config.height !== undefined) this._height = config.height;
 
         if (config.dataMapper !== undefined) this._dataMapper = config.dataMapper;
 
         if (config.onChange != undefined) this._onChange = config.onChange;
         else this._onChange = (v : KeyValue) => console.log("Valore selezionato " + v.value);
     }
+
+    get value1() : T { return this._value1; }
+
+    get value2() : T { return this._value2; }
+
+    get height() : number { return this._height; }
+
+    get dataMapper() : (value : T) => KeyValue { return this._dataMapper; }
+
+    get onChange() : (keyValue : KeyValue) => void { return this._onChange; }
 }
--- src/toggle/toggle.ts
+++ src/toggle/toggle.ts
@@ -1,26 +1,107 @@
 import ToggleConfiguration from "./toggle-configuration";
 import "./toggle.scss";
+import KeyValue from "../key-value";
 
 export default class Toggle<T>
 {
     private _element : HTMLElement;
     private _configuration : ToggleConfiguration<T>;
-    private _toggleElement : JQuery<HTMLInputElement> = $('<input />', {type: 'checkbox'});
+    private _mainDimension : number;
+    private _value1 : KeyValue;
+    private _value2 : KeyValue;
+    private _selected : KeyValue;
+
+    private _toggleElement : JQuery<HTMLDivElement> = $('<div />');
+    private _toggleButtonElement : JQuery<HTMLButtonElement> = $('<button />');
+    private _toggleIconElement : JQuery<HTMLDivElement> = $('<div />');
+    private _toggleFirstValueElement : JQuery<HTMLSpanElement> = $('<span />');
+    private _toggleSecondValueElement : JQuery<HTMLSpanElement> = $('<span />');
 
     constructor(element: HTMLElement, config : ToggleConfiguration<T>)
     {
         this._element = element;
         this._configuration = config;
+        this._mainDimension = config.height;
+        this._value1 = config.dataMapper(config.value1);
+        this._value2 = config.dataMapper(config.value2);
+        this._selected = this._value1;
 
         this.render();
     }
 
     private render() : void
     {
+        this.setDimensions();
+
+        this._toggleIconElement
+            .addClass('toggle-icon');
+
+        this._toggleButtonElement
+            .addClass('toggle-button')
+            .addClass('left')
+            .append(this._toggleIconElement)
+            .click(() => this.toggle());
+
+        this._toggleFirstValueElement
+            .addClass('toggle-label')
+            .addClass('selected')
+            .text(this._value1.value);
+
+        this._toggleSecondValueElement
+            .addClass('toggle-label')
+            .text(this._value2.value);
+        
         this._toggleElement
             .addClass('toggle')
-            .append($('<span />').append('test'));
+            .append(this._toggleFirstValueElement)
+            .append(this._toggleButtonElement)
+            .append(this._toggleSecondValueElement);
 
         $(this._element).append(this._toggleElement);
     }
+
+    private setDimensions() : void
+    {
+        this._toggleButtonElement
+            .css('width', this._mainDimension * 2 + 'px')
+            .css('height', this._mainDimension + 'px')
+            .css('border-radius', this._mainDimension / 2 + 'px');
+
+        this._toggleIconElement
+            .css('width', this._mainDimension - 4 + 'px')
+            .css('height', this._mainDimension - 4 + 'px');
+
+        if ( this._mainDimension > 25 ) // sotto a 25px come mainDimension il testo diventa troppo poco visibile
+        {
+            this._toggleFirstValueElement.css('font-size', this._mainDimension / 2.5 + 'px')
+            this._toggleSecondValueElement.css('font-size', this._mainDimension / 2.5 + 'px')
+        }
+        else
+        {
+            this._toggleFirstValueElement.css('font-size', 12 + 'px')
+            this._toggleSecondValueElement.css('font-size', 12 + 'px')
+        }
+    }
+
+    private toggle() : void
+    {
+        this._selected = this._selected == this._value1 ? this._value2 : this._value1;
+        this._toggleFirstValueElement.toggleClass('selected');
+        this._toggleSecondValueElement.toggleClass('selected');
+        this._toggleButtonElement.toggleClass('left');
+        this._toggleButtonElement.toggleClass('right');
+
+        if ( this._selected == this._value2 )
+            this._toggleIconElement.css('left', this._mainDimension + 2 + 'px');
+        else
+            this._toggleIconElement.css('left', 2 + 'px');  
+
+        this._configuration.onChange(this._selected);
+    }
+
+    public setFirstValue() : void { if ( this._selected != this._value1 ) this.toggle(); }
+
+    public setSecondValue() : void { if ( this._selected != this._value2 ) this.toggle(); }
+
+    public getValue() : T { return this._toggleButtonElement.hasClass('left') ? this._configuration.value1 : this._configuration.value2; }
 }
